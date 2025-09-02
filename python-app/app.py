e1365f6f83f1fbbed52cc2135b682a9b57f62eeb
pooja bisht <poojabisht@poojas-MacBook-Pro.local>
2025-09-02 18:13:01 +0530

    adding python app

---
+++ python-app/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Title
+st.title("🚀 My First Streamlit App")
+
+# Input text
+name = st.text_input("Enter your name:")
+
+# Button + Output
+if st.button("Greet Me"):
+    st.success(f"Hello, {name}! Welcome to Streamlit 🎉")
+
+# Slider
+age = st.slider("Select your age:", 1, 100, 25)
+st.write(f"You are {age} years old.")
