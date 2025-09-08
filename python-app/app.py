66722015fc299a582ec12ab3a480efe90d10bba8
pooja bisht <poojabisht@poojas-MacBook-Pro.local>
2025-09-08 11:59:28 +0530

    testing db connection

--- python-app/app.py
+++ python-app/app.py
@@ -1,16 +1,17 @@
 import streamlit as st
+from db_config import insert_user, get_all_users
 
-# Title
-st.title("🚀 My First Streamlit App")
+st.title("🚀 My First Streamlit App with MySQL")
 
-# Input text
 name = st.text_input("Enter your name:")
-
-# Button + Output
-if st.button("Greet Me"):
-    st.success(f"Hello, {name}! Welcome to Streamlit 🎉")
-
-# Slider
 age = st.slider("Select your age:", 1, 100, 25)
 st.write(f"You are {age} years old.")
 
+if st.button("Greet Me"):
+    if name.strip():
+        if insert_user(name, age):  
+            st.success(f"Hello, {name}! Welcome to Streamlit 🎉")
+        else:
+            st.error("❌ Failed to save user.")
+    else:
+        st.warning("⚠️ Name cannot be empty.")
